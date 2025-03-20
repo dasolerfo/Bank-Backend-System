c1d73a7588ad1099ab8074d1b863575ea4bc71eb
Daniel Soler <dasolerfo@gmail.com>
2025-03-21 04:43:08 +0900

    init CI workflow

--- db/model/store.go
+++ db/model/store.go
@@ -46,8 +46,8 @@ type TransferTxResult struct {
 	Transfer    Transfer `json:"transfer"`
 	FromAccount Account  `json:"from_account"`
 	ToAccount   Account  `json:"to_account"`
-	FromEntry   Entry    `json: from_entry`
-	ToEntry     Entry    `json: to_entry`
+	FromEntry   Entry    `json:"from_entry"`
+	ToEntry     Entry    `json:"to_entry"`
 }
 
 var txKey = struct{}{}
